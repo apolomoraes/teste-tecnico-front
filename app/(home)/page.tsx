22d8e128e26b1cfbf38563919ea7fa79f30fc6ed
Apolo Moraes <vlogsapolo@gmail.com>
2024-02-28 16:35:46 -0300

    feat: added basic table structure and starting table content component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,20 @@
 const Home = () => {
   return ( 
-    <>
-      
-    </>
+    <main>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>car</th>
+              <th>next reservation</th>
+              <th>status</th>
+            </tr>
+          </thead>
+
+          {/* tbody */}
+        </table>
+      </div>
+    </main>
   );
 }
  
