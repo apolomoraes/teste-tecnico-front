bea7ff8ea1146fe8e788a457f48878f7c077c532
Apolo Moraes <vlogsapolo@gmail.com>
2024-02-28 22:13:41 -0300

    mobile table added and started desktop table

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,25 @@
 import Rating from "../_components/rating";
+import TableContent from "../_components/table-content";
 
 const Home = () => {
   return ( 
     <main>
-      <div>
-        <Rating rating={4} />
+      <div className="min-h-screen flex justify-center items-center">
         <table>
           <thead>
-            <tr className="hidden md:block">
+            <tr className="hidden md:flex gap-48">
               <th>car</th>
               <th>next reservation</th>
               <th>status</th>
+              <th>rating</th>
+              <th>actions</th>
             </tr>
           </thead>
-
-          
+          <tbody>
+            <tr className="flex flex-col gap-4">
+              <TableContent />
+            </tr>  
+          </tbody>          
         </table>
       </div>
     </main>
--- app/_components/table-content.tsx
+++ app/_components/table-content.tsx
@@ -1,12 +1,38 @@
+"use client"
+
+import Image from "next/image";
+import Rating from "./rating";
+import { SlOptionsVertical } from "react-icons/sl";
+
 const TableContent = () => {
   return (
-    <tbody>
-      <tr>
-        <td>
-          <img src="" alt="" />
-        </td>
-      </tr>
-    </tbody>
+    <>
+      <td className="flex gap-5 border-b border-gray-300 pb-2 md:hidden">
+      <div className="flex items-end gap-3">
+        <div className="relative h-[5rem] w-[6rem]">
+          <Image src="/car.png" fill alt="imagem"/>
+        </div>
+
+        <div className="flex flex-col">
+          <h2 className="mt-[0.5rem] font-bold text-lg">Mini Cooper 2020</h2>
+          <p className="mb-[0.8rem] text-sm text-gray-400">Next reservation: 11/07</p>
+          <Rating rating={5} />
+        </div>
+      </div>
+
+      <SlOptionsVertical className="flex content-start"/>
+    </td>
+
+      <td className="hidden md:block">
+        <div className="flex items-center gap-[0.5rem]">
+          <div className="relative h-[3rem] w-[3rem] ">
+            <Image src="/car.png" fill alt="imagem"/>
+          </div>
+
+          <h2>Mini Cooper</h2>
+        </div>
+      </td>
+    </>
   );
 }
  
